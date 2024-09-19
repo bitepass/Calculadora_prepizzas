d0806fc6c308e55f88d8dacfac731fabeb48e5bc
BOTELLADECRISTAL <129355530+BOTELLADECRISTAL@users.noreply.github.com>
2024-09-19 00:45:19 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,44 @@
-const CACHE_NAME = 'prepizzas-calculator-v1';
+const CACHE_NAME = 'calculadora-prepizzas-v1';
 const urlsToCache = [
   '/Calculadora_prepizzas/',
   '/Calculadora_prepizzas/index.html',
   '/Calculadora_prepizzas/manifest.json',
-  '/Calculadora_prepizzas/icon-192x192.png',
-  '/Calculadora_prepizzas/icon-512x512.png'
+  '/Calculadora_prepizzas/icono_192x192.png',
+  '/Calculadora_prepizzas/icono_512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
